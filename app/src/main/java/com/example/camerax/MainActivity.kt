816487b56213679dfcc1284963991e88d00782ee
Youssef_Elfeky <youssefelfeky83@gmail.com>
2024-03-18 14:07:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/camerax/MainActivity.kt
@@ -0,0 +1,261 @@
+package com.example.camerax
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture.OnImageCapturedCallback
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.video.FileOutputOptions
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoRecordEvent
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.video.AudioConfig
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cameraswitch
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material.icons.filled.Videocam
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.camerax.ui.theme.CameraXTheme
+import kotlinx.coroutines.launch
+import java.io.File
+
+class MainActivity : ComponentActivity() {
+
+    private var recording:Recording? = null
+
+    @SuppressLint("RememberReturnType")
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (!hasRequiredPermissions()){
+            ActivityCompat.requestPermissions(this,cameraPermissions,0)
+        }
+        setContent {
+            CameraXTheme {
+                val scope = rememberCoroutineScope()
+                val scaffoldState = rememberBottomSheetScaffoldState()
+                val controller = remember{
+                    LifecycleCameraController(applicationContext).apply {
+                        setEnabledUseCases(
+                            CameraController.IMAGE_CAPTURE or
+                            CameraController.VIDEO_CAPTURE
+                        )
+                    }
+                }
+                val viewModel = viewModel<MainViewModel>()
+                val bitmaps by viewModel.bitmaps.collectAsState()
+
+                BottomSheetScaffold(
+                    sheetContent = {
+                                   PhotoBottomSheetContent(
+                                       bitmaps = bitmaps,
+                                       modifier = Modifier
+                                           .fillMaxWidth()
+                                   )
+                    },
+                    scaffoldState = scaffoldState,
+                    sheetPeekHeight = 0.dp
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(it)
+                    ) {
+                        CameraPreview(
+                            controller = controller,
+                            modifier = Modifier.fillMaxSize()
+                        )
+
+                        IconButton(
+                            onClick = {
+                            controller.cameraSelector =
+                                if (controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA)
+                                CameraSelector.DEFAULT_FRONT_CAMERA
+                                else CameraSelector.DEFAULT_BACK_CAMERA
+
+                            },
+                            modifier = Modifier
+                                .offset(16.dp,16.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Cameraswitch,
+                                contentDescription = "switch camera"
+                            )
+                        }
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .align(Alignment.BottomCenter)
+                                .padding(16.dp),
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
+                            IconButton(
+                                onClick = {
+                                    scope.launch {
+                                        scaffoldState.bottomSheetState.expand()
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Photo,
+                                    contentDescription = "open gallery"
+                                )
+                            }
+                            IconButton(
+                                onClick = {
+                                    takePhoto(
+                                        controller,
+                                        onPhotoTaken = viewModel::onTakePhoto
+                                        )
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.PhotoCamera,
+                                    contentDescription = "take photo"
+                                )
+                            }
+                            IconButton(
+                                onClick = {
+                                    recordVideo(controller)
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Videocam,
+                                    contentDescription = "record video"
+                                )
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun takePhoto(
+        controller: LifecycleCameraController,
+        onPhotoTaken: (Bitmap) -> Unit
+    ){
+        if (!hasRequiredPermissions()){
+            return
+        }
+        controller.takePicture(
+            ContextCompat.getMainExecutor(applicationContext),
+            object : OnImageCapturedCallback(){
+                override fun onCaptureSuccess(image: ImageProxy) {
+                    super.onCaptureSuccess(image)
+
+                    val matrix = Matrix().apply {
+                        postRotate(image.imageInfo.rotationDegrees.toFloat())
+                        postScale(-1f,1f)
+                    }
+                    val rotatedBitmap = Bitmap.createBitmap(
+                        image.toBitmap(),
+                        0,
+                        0,
+                        image.width,
+                        image.height,
+                        matrix,
+                        true
+                    )
+
+                    onPhotoTaken(rotatedBitmap)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    super.onError(exception)
+                    Log.e("camera","couldn't take photo: ",exception)
+                }
+            }
+        )
+    }
+
+    val outputFile = File(filesDir,"my-recording.mp4")
+
+    @SuppressLint("MissingPermission")
+    private fun recordVideo(
+        controller: LifecycleCameraController
+    ){
+        if (recording != null){
+            recording?.stop()
+            recording = null
+            return
+        }
+        if (!hasRequiredPermissions()){
+            return
+        }
+        recording = controller.startRecording(
+            FileOutputOptions.Builder(outputFile).build(),
+            AudioConfig.create(true),
+            ContextCompat.getMainExecutor(applicationContext)
+        ){event ->
+            when(event){
+                is VideoRecordEvent.Finalize ->{
+                    if (event.hasError()){
+                        recording?.close()
+                        recording = null
+                        Toast.makeText(applicationContext,"Video capture failed",Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(applicationContext,"Video capture succeeded",Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+
+        }
+
+    }
+
+
+    private fun hasRequiredPermissions():Boolean{
+        return cameraPermissions.all {
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                it
+            )== PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+
+    companion object{
+        private val cameraPermissions = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+        )
+    }
+}
+
